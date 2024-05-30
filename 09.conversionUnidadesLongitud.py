0f6ff0d612cda0ccd00273532ecbb678098240cd
jorgelpat <156463233+jorgelpat@users.noreply.github.com>
2024-05-30 15:54:46 -0500

    feat: :sparkles: Unidades de longitud
    se hace conversion de un dato en cm y se pasa a pulgadas

---
+++ 09.conversionUnidadesLongitud.py
@@ -0,0 +1,6 @@
+# Escriba un programa que convierta de centímetros a pulgadas. Una pulgada es igual a 2.54 centímetros.
+
+cm = float(input("Escriba una longitud en centimetros: "))
+pulg = cm/2.54
+
+print(f"{cm}cm son {pulg}in")
