f0b32a0f1ca3573c9f17440448623de3e7e9cf50
jorgelpat <156463233+jorgelpat@users.noreply.github.com>
2024-06-04 20:22:41 -0500

    feat: :sparkles: Se crea programa de hora futura

--- 12.horaFutura.py
+++ 12.horaFutura.py
@@ -1,2 +1,12 @@
 # Escriba un programa que pregunte al usuario la hora actual t del reloj 
 # y un número entero de horas h, que indique qué hora marcará el reloj dentro de h horas:
+
+t = int(input("Hora actual: "))
+h = int(input("Cantidad de horas: "))
+
+suma = t+h
+
+while suma > 12:
+    suma = suma - 12
+
+print(f"En {h} horas el reloj marcará las {suma}")
